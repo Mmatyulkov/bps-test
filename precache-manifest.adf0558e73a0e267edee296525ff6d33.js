b211bfa86297165e9831524104f03c8cb4107bd0
Mmatyulkov <Mmatyulkov@users.noreply.github.com>
2020-09-10 01:10:33 +0300

    Updates

---
+++ precache-manifest.adf0558e73a0e267edee296525ff6d33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887720bddf82a1ad2ff754dfccbacab4",
+    "url": "/bps-test/index.html"
+  },
+  {
+    "revision": "aafac63c0123776b8c3b",
+    "url": "/bps-test/static/css/main.eb7b42af.chunk.css"
+  },
+  {
+    "revision": "560195067d6aeab806aa",
+    "url": "/bps-test/static/js/2.730fa4ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bps-test/static/js/2.730fa4ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aafac63c0123776b8c3b",
+    "url": "/bps-test/static/js/main.dfb696c8.chunk.js"
+  },
+  {
+    "revision": "df450b93e25e89e838ba",
+    "url": "/bps-test/static/js/runtime-main.33ff3651.js"
+  },
+  {
+    "revision": "046c8985e683961d0c6af0c2337f1e29",
+    "url": "/bps-test/static/media/broccoli_img (1).046c8985.png"
+  },
+  {
+    "revision": "4f740ca196ec86f7f46fc36ed278f884",
+    "url": "/bps-test/static/media/broccoli_img.4f740ca1.png"
+  }
+]);
